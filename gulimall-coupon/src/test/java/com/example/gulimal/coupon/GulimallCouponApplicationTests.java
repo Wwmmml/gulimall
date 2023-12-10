5ff0aaa5433c0b1a3221585e1ed673cf8d74eef9
wangminglun <337844955@qq.com>
2023-12-10 16:27:44 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/example/gulimal/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gulimal.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
